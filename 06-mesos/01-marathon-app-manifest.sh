dca1b7bc3116e6f70ee2d5de6c558e8c3477f08b
Viet Nguyen <inbox@dev-forge.ru>
2017-04-17 00:49:15 +0300

    feat: deploy mesos

---
+++ 06-mesos/01-marathon-app-manifest.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+set -x
+
+cat << EOF > pikachu-server-manifest.json
+[
+  {
+    "id": "pikachu-server",
+
+    "cpus": 0.1,
+    "mem": 128,
+    "instances": 2,
+
+    "container": {
+      "type": "DOCKER",
+      "docker": {
+        "image": "0x06065a/pikachu-server",
+        "network": "BRIDGE",
+        "portMappings": [{
+            "containerPort": 8080
+        }]
+      }
+    },
+
+    "healthChecks": [
+      {
+        "gracePeriodSeconds": 300,
+        "intervalSeconds": 5,
+        "maxConsecutiveFailures": 3,
+        "path": "/",
+        "portIndex": 0,
+        "protocol": "HTTP",
+        "timeoutSeconds": 10
+      }
+    ],
+
+    "upgradeStrategy": {
+      "maximumOverCapacity": 0.5,
+      "minimumHealthCapacity": 1.0
+    }
+  }
+]
+EOF
