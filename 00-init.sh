a6c2a84544f55bebe94795ea264cabdb7cc66b69
Viet Nguyen <inbox@dev-forge.ru>
2017-04-17 11:18:23 +0300

    fix: set proper vagrant box

--- 00-init.sh
+++ 00-init.sh
@@ -2,7 +2,6 @@
 
 set -x
 
-docker pull nginx:1.10.3-alpine
 docker pull python:3.6.1-alpine
 
-vagrant box add ubuntu/xenial64 --box-version 20170414.2.0
+vagrant box add stereohorse/alfa-infra-sandbox
